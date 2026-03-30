73857541bb3c14f07116c282b145651586d51557
Kairos <144052061+brunoagmoreira@users.noreply.github.com>
2026-03-30 16:04:44 -0300

    fix(api): NUCLEAR FIX for 502/Failed to fetch with Debian and full root build context

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
 
   const port = process.env.PORT ?? 3001;
   await app.listen(port, '0.0.0.0');
-  console.log(`🚀 API rodando em http://0.0.0.0:${port}/api`);
+  console.log(`\n************************************\n🚀 API Seu Novo Carro ONLINE\n🚀 Ouvindo em: http://0.0.0.0:${port}/api\n************************************\n`);
 }
 bootstrap();
